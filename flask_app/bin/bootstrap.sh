dc0d3c27fb4c84bcb023b0be6038fb3ac4f5c97b
southeo <soulaine.theo@gmail.com>
2022-10-05 15:11:32 +0200

    fixing dockerfile entrypoint

--- flask_app/bin/bootstrap.sh
+++ flask_app/bin/bootstrap.sh
@@ -2,5 +2,5 @@
 
 #export FLASK_APP=./app.py
 #source $(pip --venv)/bin/activate
-cd ../
+#cd ../  #uncomment this only to run from wsgi.py!!! This should remain commented when containerized
 gunicorn wsgi:app --bind 0.0.0.0:5000 --workers=4
