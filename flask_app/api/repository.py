f7e9bda5e2eb3afd1185977386dac0ec85d82237
southeo <soulaine.theo@gmail.com>
2023-01-26 10:52:48 +0100

    import changes

--- flask_app/api/repository.py
+++ flask_app/api/repository.py
@@ -3,11 +3,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import text
 
-#from bin import db_connections
-#from api import Tables as db
+from bin import db_connections
+from api import Tables as db
 
-from flask_app.bin import db_connections
-from flask_app.api import Tables as db
+#from flask_app.bin import db_connections
+#from flask_app.api import Tables as db
 
 def flatten(l):
     # Flatten a list (usually the results of a sqlalchemy query)
